d3df50a0416a2cdf7755d561916d807bb159a417
arthurqchu <arthurqc@gmail.com>
2019-09-19 18:40:19 -0500

    9/19 6:40 PM

--- Lab_3/Shape.java
+++ Lab_3/Shape.java
@@ -1,1 +1,18 @@
-hello
+/**
+ * Shape class with area, perimeter, and getShape methods
+ */
+
+public abstract class Shape {
+	private String name;
+	private static int shapeCount;
+
+	//Abstract Methods	
+	abstract double area();
+	abstract double perimeter();
+	abstract String getShape();
+
+	//Constructor
+	public Shape(String name) {
+		this.name = name;
+	}
+}
