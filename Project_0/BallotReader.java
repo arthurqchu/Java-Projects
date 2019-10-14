143c644798c52607c9facd7c069cab7bb9959595
arthurqchu <arthurqc@gmail.com>
2019-10-14 09:58:05 -0500

    FINISH

--- Project_0/BallotReader.java
+++ Project_0/BallotReader.java
@@ -1,4 +1,6 @@
 import java.io.IOException;
+import java.io.FileReader;
+
 
 
 public class BallotReader
@@ -9,11 +11,20 @@ public class BallotReader
 	{
 		// ***Replace this with the correct implementation; for now, it's
 		//    always returning a hard-coded ballot with three candidates.
-		Ballot ballot = new Ballot("James Beard Award");
-		ballot.addCandidate(new Candidate("Nina Compton", "Compere Lapin Party"));
-		ballot.addCandidate(new Candidate("Alon Shaya", "Saba Party"));
-		ballot.addCandidate(new Candidate("Emeril Lagasse", "Emeril's Party"));
-		return ballot;
+		//Ballot ballot = new Ballot("James Beard Award");
+		//ballot.addCandidate(new Candidate("Nina Compton", "Compere Lapin Party"));
+		//ballot.addCandidate(new Candidate("Alon Shaya", "Saba Party"));
+		//ballot.addCandidate(new Candidate("Emeril Lagasse", "Emeril's Party"));
+		//return ballot;
+
+		//Read the input file
+		String fileName;
+		String inputInfo;
+		FileReader inputFile;
+
+		inputFile = new FileReader(fileName);
+		inputInfo = inputFile.read();
+
 	}
 }
 
--- Project_0/ResultWriter.java
+++ Project_0/ResultWriter.java
@@ -1,6 +1,7 @@
  
 
 import java.io.IOException;
+import java.io.FileWriter;
 
 
 public class ResultWriter
@@ -8,7 +9,8 @@ public class ResultWriter
 	public static void writeResults(String filename, Ballot ballot)
 	throws IOException
 	{
-
+	//Construct a long string and then write it to a file at the end
+		
 	}
 }
 
