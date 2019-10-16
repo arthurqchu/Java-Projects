96d05f8353d2dee38f324e49ccdcc649a3a0bafc
arthurqchu <arthurqc@gmail.com>
2019-10-15 19:17:21 -0500

    Do JUnit Tests

--- Project_0/Candidate.java
+++ Project_0/Candidate.java
@@ -1,41 +1,67 @@
+/**
+ * Candidate class constructs Candidate objects that contain each candidate's name and affiliation
+ * @author Arthur
+ *
+ */
+
 public class Candidate
 {
 	//Variables
 	private String name;
 	private String affiliation;
-
+	private int voteCount;
+	
+	/**
+	 * Candidate constructor takes in name of affiliation
+	 * @param name
+	 * @param affiliation
+	 */
 	public Candidate(String name, String affiliation)
 	{
 		this.name = name;
 		this.affiliation = affiliation;
 	}
 
-
+	/**
+	 * Returns name of Candidate
+	 * @return this.name
+	 */
 	public String getName()
 	{
     		return this.name;
 	}
 
-
+	/**
+	 * Returns affiliation of Candidate
+	 * @return this.affiliation
+	 */
 	public String getAffiliation()
 	{
 		 return this.affiliation;
 	}
 
-
+	/**
+	 * Returns vote count for the candidate
+	 * @return this.vote
+	 */
 	public int getVoteCount()
 	{
-    		return -1;
+    	return this.voteCount;
 	}
 
+	/**
+	 * Increments the Candidate's voteCount
+	 */
 	public void tallyVote()
 	{
-
+		this.voteCount += 1;
 	}
 
-
+	/**
+	 * Displays all inforamtion about the Candidate
+	 */
 	public String toString()
 	{
-		 return this.getName() + " - " + this.getAffiliation();
+		 return "Name: " + getName() + '\n' + "Affiliation: " + getAffiliation() + '\n' + "Number of Votes: " + getVoteCount();
 	}
 }
--- Project_0/ResultWriter.java
+++ Project_0/ResultWriter.java
@@ -2,6 +2,7 @@
 
 import java.io.IOException;
 import java.io.FileWriter;
+import java.io.File;
 import java.util.ArrayList;
 
 public class ResultWriter
@@ -11,24 +12,47 @@ public class ResultWriter
 	{
 	//Construct a long string and then write it to a file at the end
 		ballot = BallotReader.readBallot(filename);
+		String header = "";
+		String dashes = "";
+		String candidateTags = "";
+		String outcome = "";
+		String outputString = "";
 
-		String results = String.format("RESULTS - %s", ballot.getOfficeName());
-
-		//Add the correct number of dashes
-		for(int i = 0; i < FIXME; i++) {
-			results += '-';
-		}
-		results += '\n';
+		header = String.format("RESULTS - %s", ballot.getOfficeName());
 
 		//Determine the longest tag 
 		String longestTag = "";
 		for(Candidate candidate : ballot.getCandidates()) {
 			if(candidate.getName().length() + candidate.getAffiliation().length() > longestTag.length()) {
-				longestTag = candidate.toString();
+				longestTag = candidate.getName() + " - " + candidate.getAffiliation();
 			}
-			String resultFormat = "%-s" + "%" + longestTag.length() + "+d");
-			results += String.format(resultFormat, candidate.toString(), candidate.getVoteCount());
+			String resultFormat = "%-s" + "%" + (longestTag.length() + 12) + "+d";
+			candidateTags += String.format(resultFormat,  candidate.getName() + " - " + candidate.getAffiliation(), candidate.getVoteCount());
 		}
+
+		//Add the correct number of dashes
+		for(int i = 0; i < longestTag.length(); i++) {
+			dashes += '-';
+		}
+		//Determine who has the most votes
+		String winner = "";
+		int mostVotes = 0;
+		for(Candidate candidate : ballot.getCandidates()) {
+			if(candidate.getVoteCount() > mostVotes) {
+				winner = candidate.getName() + " - " + candidate.getAffiliation();
+			}
+		}
+		if(winner == "") {
+			winner = "NO WINNER";
+		}
+		outcome += String.format("WINNER: %s", winner);
+
+		outputString += header + '\n' + dashes + '\n' + candidateTags + '\n' + outcome;
+
+		File file = new File("results.txt");
+		FileWriter writer = new FileWriter(file);
+		writer.write(outputString);
+		writer.close();
 	}
 }
 
